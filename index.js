fce7ea0162afd4f21b0e664bafa70ba370ab62d1
shubhmane99 <69582236+shubhmane99@users.noreply.github.com>
2025-06-06 17:27:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node.js API!' });
